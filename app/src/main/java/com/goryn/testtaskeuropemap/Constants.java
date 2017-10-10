6b0dd1575ab63893b71443c5f71ee474e9cab54e
goryn.alexander <goryn.alexander@gmail.com>
2017-10-11 00:07:09 +0300

    Обработка карты, клик определяет страну и выводит диалог

---
+++ app/src/main/java/com/goryn/testtaskeuropemap/Constants.java
@@ -0,0 +1,57 @@
+package com.goryn.testtaskeuropemap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Constants {
+    public static final Map<String, String> COUNTRIES_MAP = new HashMap<>();
+    static {
+        COUNTRIES_MAP.put("#f46d43", "Russia");
+        COUNTRIES_MAP.put("#7171c6", "Ukraine");
+        COUNTRIES_MAP.put("#ffff33", "Slovakia");
+        COUNTRIES_MAP.put("#8c6bb1", "Georgia");
+        COUNTRIES_MAP.put("#b2182b", "Azerbaijan");
+        COUNTRIES_MAP.put("#feb24c", "Armenia");
+        COUNTRIES_MAP.put("#004529", "Turkey");
+        COUNTRIES_MAP.put("#cc3333", "Cyprus");
+        COUNTRIES_MAP.put("#99d8c9", "Greece");
+        COUNTRIES_MAP.put("#b30000", "Bulgaria");
+        COUNTRIES_MAP.put("#4d004b", "Moldavia");
+        COUNTRIES_MAP.put("#006d2c", "Poland");
+        COUNTRIES_MAP.put("#cab2d6", "Norway");
+        COUNTRIES_MAP.put("#525252", "Sweden");
+        COUNTRIES_MAP.put("#e41a1c", "Finland");
+        COUNTRIES_MAP.put("#f7fcb9", "Estonia");
+        COUNTRIES_MAP.put("#c7eae5", "Latvia");
+        COUNTRIES_MAP.put("#800026", "Lithuania");
+        COUNTRIES_MAP.put("#dfc27d", "Belarus");
+        COUNTRIES_MAP.put("#fdbb84", "Spain");
+        COUNTRIES_MAP.put("#969696", "Portugal");
+        COUNTRIES_MAP.put("#fc4e2a", "France");
+        COUNTRIES_MAP.put("#bfd3e6", "GreatBritain");
+        COUNTRIES_MAP.put("#ffffbf", "Italy");
+        COUNTRIES_MAP.put("#238443", "Bosnia and Herzegovina");
+        COUNTRIES_MAP.put("#74a9cf", "Romania");
+        COUNTRIES_MAP.put("#4393c3", "Austria");
+        COUNTRIES_MAP.put("#e5f5f9", "Germany");
+        COUNTRIES_MAP.put("#ffa07a", "Croatia");
+        COUNTRIES_MAP.put("#fdb462", "Albania");
+        COUNTRIES_MAP.put("#f7f7f7", "Belgium");
+        COUNTRIES_MAP.put("#e31c2c", "Netherlands");
+        COUNTRIES_MAP.put("#01665e", "Denmark");
+        COUNTRIES_MAP.put("#ff7f00", "Czech Republic");
+        COUNTRIES_MAP.put("#ef6548", "Switzerland");
+        COUNTRIES_MAP.put("#8c510a", "Kosovo");
+        COUNTRIES_MAP.put("#4575b4", "FYROM");
+        COUNTRIES_MAP.put("#7f0000", "Slovenia");
+        COUNTRIES_MAP.put("#41ae76", "Montenegro");
+        COUNTRIES_MAP.put("#71c671", "Serbia");
+        COUNTRIES_MAP.put("#33a02c", "Hungary");
+        COUNTRIES_MAP.put("#0868ac", "Ireland");
+        COUNTRIES_MAP.put("#fddbc7", "Iceland");
+
+
+    }
+
+}
--- app/src/main/java/com/goryn/testtaskeuropemap/MainActivity.java
+++ app/src/main/java/com/goryn/testtaskeuropemap/MainActivity.java
@@ -1,23 +1,40 @@
 package com.goryn.testtaskeuropemap;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.Matrix;
+import android.graphics.Point;
 import android.graphics.drawable.BitmapDrawable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ScrollView;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private HorizontalScrollView svHorizontal;
+    private ScrollView svVertical;
+    private Map<String, String> countriesMap;
+    boolean firstLaunch = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        countriesMap = Constants.COUNTRIES_MAP;
+
 
         ImageView imageView = (ImageView) findViewById(R.id.iv_map);
 
@@ -25,36 +42,80 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onTouch(View view, MotionEvent event) {
 
-                ImageView image = (ImageView) view;
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        return true;
 
-                Matrix inverse = new Matrix();
-                image.getImageMatrix().invert(inverse);
+                    case MotionEvent.ACTION_UP:
+                        ImageView image = (ImageView) view;
 
-// map touch point from ImageView to image
-                float[] touchPoint = new float[] {event.getX(), event.getY()};
-                inverse.mapPoints(touchPoint);
-
-
-                Bitmap bitmap = ((BitmapDrawable) image.getDrawable()).getBitmap();
-                int pixel = bitmap.getPixel((int) touchPoint[0],(int) touchPoint[1]);
-
-                //then do what you want with the pixel data, e.g
-                int redValue = Color.red(pixel);
-                int blueValue = Color.blue(pixel);
-                int greenValue = Color.green(pixel);
-
-                Log.i("COLRO", "" + redValue + " " + greenValue + " " + blueValue);
-                Log.i("COLRO", fromRGBToHex(redValue, greenValue, blueValue));
+                        Matrix inverse = new Matrix();
+                        image.getImageMatrix().invert(inverse);
 
 
+                        float[] touchPoint = new float[]{event.getX(), event.getY()};
+                        inverse.mapPoints(touchPoint);
+
+
+                        Bitmap bitmap = ((BitmapDrawable) image.getDrawable()).getBitmap();
+                        int pixel = bitmap.getPixel((int) touchPoint[0], (int) touchPoint[1]);
+
+                        //then do what you want with the pixel data, e.g
+                        int redValue = Color.red(pixel);
+                        int blueValue = Color.blue(pixel);
+                        int greenValue = Color.green(pixel);
+
+                        Log.i("COLOR", "" + redValue + " " + greenValue + " " + blueValue);
+                        Log.i("COLOR", fromRGBToHex(redValue, greenValue, blueValue));
+                        checkColor(fromRGBToHex(redValue, greenValue, blueValue));
+                }
                 return false;
             }
         });
 
     }
 
-    private String fromRGBToHex(int r, int g, int b){
-        return String.format( "#%02x%02x%02x", r, g, b );
+    private void checkColor(String hex) {
+        if (countriesMap.containsKey(hex)) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(countriesMap.get(hex));
+            builder.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+            Dialog dialog = builder.create();
+            dialog.show();
+        }
+    }
+
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        if (firstLaunch){
+            centerScrollView();
+            firstLaunch = false;
+        }
+
+        super.onWindowFocusChanged(hasFocus);
+    }
+
+    private String fromRGBToHex(int r, int g, int b) {
+        return String.format("#%02x%02x%02x", r, g, b);
+    }
+
+
+    private void centerScrollView() {
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+
+        svHorizontal = (HorizontalScrollView) findViewById(R.id.sv_horizontal);
+        svVertical = (ScrollView) findViewById(R.id.sv_vertical);
+
+        svVertical.scrollTo(0, size.y / 2);
+        svHorizontal.scrollTo(size.x / 2, 0);
     }
 
 }
